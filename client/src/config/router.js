8be4c3d7ccfe93b9fd0341877f019c365456ee3a
KieranApps <kieran.apps99@gmail.com>
2024-05-10 14:25:28 +0100

    Create base for content pages

--- client/src/config/router.js
+++ client/src/config/router.js
@@ -1,20 +1,43 @@
 import { lazy } from 'react';
 
+import ROUTES from './routes.js';
+
 const Home = lazy(() => import('../components/Home/home'));
 const Login = lazy(() => import('../components/Login/login'));
+const Objectives = lazy(() => import('../components/Objectives/objectives.jsx'));
+const Projects = lazy(() => import('../components/Projects/projects.jsx'))
+const HR = lazy(() => import('../components/HR/hr.jsx'))
 
 // eslint-disable-next-line
 export default {
     home: {
-        path: '/',
+        path: ROUTES.HOME,
         exact: true,
         component: Home,
         private: false 
     }, 
     login: {
-        path: '/login',
+        path: ROUTES.LOGIN,
         exact: true,
         component: Login,
         private: false 
+    },
+    objectives: {
+        path: ROUTES.OBJECTIVES,
+        exact: true,
+        component: Objectives,
+        private: false
+    },
+    projects: {
+        path: ROUTES.PROJECTS,
+        exact: true,
+        component: Projects,
+        private: false
+    },
+    hr: {
+        path: ROUTES.HR,
+        exact: true,
+        component: HR,
+        private: false
     }
 };
--- client/src/config/routes.js
+++ client/src/config/routes.js
@@ -1,6 +1,9 @@
 const ROUTES = {
     HOME: '/',
     LOGIN: '/login',
+    OBJECTIVES: '/objectives',
+    PROJECTS: '/projects',
+    HR: '/hr'
     // Example with variable: PAGE: '/page/:var',
 }
 
