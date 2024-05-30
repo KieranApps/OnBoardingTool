03eecbdc0d9cc32a91c8aa449328d5bb8c593f6a
KieranApps <kieran.apps99@gmail.com>
2024-05-30 14:25:43 +0100

    Started modal layout

--- client/src/components/Modals/CreateProjectModal.jsx
+++ client/src/components/Modals/CreateProjectModal.jsx
@@ -1,25 +1,41 @@
-import { Box, Modal, Typography } from '@mui/material';
-const style = {
+import { useState } from 'react';
+import { Box, Modal, TextField } from '@mui/material';
+
+const modalStyle = {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 1000,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
-  };
+};
+
+
 export function ShowCreateProjectModal (props) {
+    const [name, setName] = useState(null);
+    const [description, setDescription] = useState(null);
+    const [toastText, setToastText] = useState(null);
+    
+    function onChangeName(e) {
+        setName(e.target.value);
+    }
+
+    function onChangeDescription(e) {
+        setDescription(e.target.value);
+    }
+
     return (<Modal open={props.open} onClose={props.handleClose} aria-labelledby="modal-modal-title"
     aria-describedby="modal-modal-description">
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+        <Box sx={modalStyle}>
+            <Box>
+                <TextField onChange={onChangeName} variant="filled" label="Name"/>
+            </Box>
+          <Box>
+            <TextField onChange={onChangeDescription} variant="filled" label="Decription" multiline rows={5}/>
+          </Box>
         </Box>
     </Modal>);
 }
