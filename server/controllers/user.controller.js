7e17314a56c946b812be067a64c4d4490ddb686b
KieranApps <kieran.apps99@gmail.com>
2024-05-30 11:25:21 +0100

    Store all sessions, do not update

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 import moment from 'moment';
 
 import { getUser } from '../services/user.service.js';
-import { createUserSession, updateUserSession, checkUserSession } from '../services/session.service.js'; 
+import { createUserSession } from '../services/session.service.js'; 
 import { validate, createUUID } from '../utils/util.js';
 import { NotFound } from '../utils/exceptions.js';
 
@@ -28,16 +28,7 @@ export async function login(req, res) {
 
     const sessionID = createUUID();
     const expiry = moment().add(30, 'days').toDate();
-    // Check if user has a session
-    // Not most effecient having two DB calls, but since its only on login, wont affect much. Plus keeps DB smaller than new sessions for every log in for every user
-    // Eventually, this may need to check device too. So a user can have two sessions open of two devices each with their own expiry, will need table updates and new compound key indentifier
-    const hasSession = await checkUserSession(user.id);
-    let session;
-    if (hasSession) {
-        session = await updateUserSession(user.id, sessionID, expiry);
-    } else {
-        session = await createUserSession(user.id, sessionID, expiry);
-    }
+    await createUserSession(user.id, sessionID, expiry);
     req.session.details = {
         userId: user.id,
         id: sessionID, 
--- server/services/session.service.js
+++ server/services/session.service.js
@@ -1,24 +1,8 @@
 import myknex from '../../knexConfig.js';
 
-export async function checkUserSession(id) {
-    try {
-        return myknex('sessions').select('*').where('user_id', '=', id).first();
-    } catch (error) {
-        return error;
-    }
-}
-
 export async function createUserSession(id, session_id, expiry) {
     try {
         return myknex('sessions').insert({ user_id: id, session_id, expiry });
-    } catch (error) {
-        return error;
-    }
-}
-
-export async function updateUserSession(id, session_id, expiry) {
-    try {
-        return myknex('sessions').where('user_id', '=', id).update({ user_id: id, session_id, expiry });
     } catch (error) {
         return error;
     }
