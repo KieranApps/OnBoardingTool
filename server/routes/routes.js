157e14461193c912d69b377e9875df74c07baae6
KieranApps <kieran.apps99@gmail.com>
2024-08-19 13:46:33 -0400

    Updated mql package and CORS backend error

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -28,7 +28,7 @@ const corsOptions = {
         if (whitelist.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
         } else {
-            callback(new Error('Not allowed by CORS'));
+            callback(new Error(`Origin: '${origin}' not allowed by CORS`));
         }
     },
     optionsSuccessStatus: 200,
